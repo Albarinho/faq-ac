eead9dc870b3a9d56759a31316243aeac01e7cb7
albaro claros <ac484@njit.edu>
2018-12-08 10:00:00 -0500

    Questionsview: added question route to fix error"route not define" when clicking home.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,12 +9,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+Route::resources([
+    'question' => 'QuestionController',
+]);
