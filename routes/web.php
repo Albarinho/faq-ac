20ffa6dc8ca668420f4eacc01961d9d3bcde62a1
albaro claros <ac484@njit.edu>
2018-12-12 00:00:04 -0500

    FormsPart3:Added routes to the web.php file

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->na
 
 Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
 
+Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
+Route::post('/user/{user_id}/profile/', 'ProfileController@store')->name('profile.store');
+
+Route::get('/user/{user_id}/profile/{profile_id}/edit', 'ProfileController@edit')->name('profile.edit');
+Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
+
+
 Route::resources([
     'question' => 'QuestionController',
 ]);
