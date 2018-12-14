d66905c2dce217bd7159ad5a474543c1a011ad1d
albaro claros <ac484@njit.edu>
2018-12-14 03:01:12 -0500

    FormsPart3: Answers forms - Edited answer controller

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Answer;
 use Illuminate\Support\Facades\Auth;
 use App\Question;
+
 class AnswerController extends Controller
 {
     /**
@@ -15,69 +18,107 @@ class AnswerController extends Controller
     {
         $this->middleware('auth');
     }
+
     public function index()
     {
         //
     }
+
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+
+    public function create($question)
     {
-        //
+        $answer = new Answer();
+        $edit = FALSE;
+        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question' => $question]);
     }
+
+
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Question $question)
     {
-        //
+        $input = $request->validate(
+            ['body' => 'required|min:5',],
+            ['body.required' => 'Body is required',
+                'body.min' => 'Body must be at least 5 characters',
+            ]
+        );
+        $input = request()->all();
+        $question = Question::find($question);
+        $Answer = new Answer($input);
+        $Answer->user()->associate(Auth::user());
+        $Answer->question()->associate($question);
+        $Answer->save();
+        return redirect()->route('question.show', ['question_id' => $question->id])->with('message', 'Saved');
     }
+
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($question,$answer)
+    public function show(Question $question, Answer $answer)
     {
         $answer = Answer::find($answer);
         return view('answer')->with(['answer' => $answer, 'question' => $question]);
     }
+
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $answer = Answer::find($answer);
+        $edit = TRUE;
+        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question' => $question]);
+
     }
+
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $answer = Answer::find($answer);
+        $answer->body = $request->body;
+        $answer->save();
+        return redirect()->route('answer.show', ['question_id' => $question, 'answer_id' => $answer])->with('message', 'Updated');
     }
+
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $answer = Answer::find($answer);
+        $answer->delete();
+        return redirect()->route('question.show', ['question_id' => $question])->with('message', 'Delete');
     }
 }
